037bde29bccb10cf4dd93e71d273f194c1cc9ee1
JohnDev99 <86853140+JohnDev99@users.noreply.github.com>
2022-09-08 18:05:22 +0100

    Player-Mechanics
    Player Animations Transitions

--- Dragon_Knight/Assets/Scripts/PlayerController.cs
+++ Dragon_Knight/Assets/Scripts/PlayerController.cs
@@ -10,19 +10,32 @@ public class PlayerController : MonoBehaviour
 
     float horizontalInput;
     float verticalInput;
+    [SerializeField]bool isFlipped;
 
+    Animator playerAnim;
 
     // Start is called before the first frame update
     void Start()
     {
         myRb = GetComponent<Rigidbody2D>();
+        playerAnim = GetComponentInChildren<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        if(horizontalInput != 0)
+        CheckFacingDirection();
+
+
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (horizontalInput != 0)
         {
             MovePlayer();
         }
@@ -35,16 +48,15 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    private void FixedUpdate()
+    private void LateUpdate()
     {
-
-        
+        playerAnim.SetFloat("MoveX", horizontalInput);
     }
 
     //Player começa devagar e acelera e desacelera suavemente
     void MovePlayer()
     {
-        Vector2 playerMovement = new Vector2(myRb.position.x + speed * horizontalInput * Time.deltaTime, myRb.position.y);
+        Vector2 playerMovement = new Vector2(myRb.position.x + speed * horizontalInput * Time.fixedDeltaTime, myRb.position.y);
         myRb.MovePosition(playerMovement);
         //myRb.AddForce(playerMovement);
     }
@@ -56,4 +68,31 @@ public class PlayerController : MonoBehaviour
         myRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
 
     }
+
+    void CheckFacingDirection()
+    {
+        if (horizontalInput > 0)
+        {
+            isFlipped = false;
+        }
+            
+        if(horizontalInput < 0)
+        {
+            isFlipped = true;
+        }
+
+        FlipCharacter();
+    }
+
+    void FlipCharacter()
+    {
+        //Vector3 currentScale = GetComponentInChildren<Transform>().transform.localScale;
+        //currentScale.x *= -1;
+        SpriteRenderer playerCharacter = gameObject.GetComponentInChildren<SpriteRenderer>();
+
+        playerCharacter.flipX = isFlipped;
+
+    }
+
+    
 }
